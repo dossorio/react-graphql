0620989741537ff068d639b1767058d6c05694cb
Dan Ossorio <daniel@ossorio.me>
2018-02-20 08:52:10 +0000

    Adds graphql server

---
+++ graphql/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+const root = {
+    hello: () => {
+        return 'Hello !'
+    },
+};
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.listen(4000);
+
+console.log('Running GraphQL API at localhost:4000/graphql');
